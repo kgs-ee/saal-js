b028445951f02125753d3de6b3cfcc18c27a1f60
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-04-28 11:37:44 +0300

    cleanup

--- routes/events.js
+++ routes/events.js
@@ -5,9 +5,7 @@ var router = express.Router()
 var request     = require('request')
 
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-	// var user_id = req.params.user_id
 
     var url = 'https://saal.entu.ee/api2/entity?definition=event&limit=10'
     request.get({
@@ -18,11 +16,9 @@ router.get('/', function(req, res, next) {
         	data = JSON.parse(body)
         	console.log(data.result)
         	var count = data.count
-            // var result = data.result
-            // var item = data.result[1].name
             res.render("index", {
-                count:count, 
-                title:"zaza", 
+                count:count,
+                title:"zaza",
                 events:data.result
             })
         }
