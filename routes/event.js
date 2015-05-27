cb5c21a643fd6fcc72115a02ee798d204ede3d1c
Ranno Ait <rannoait@gmail.com>
2015-05-27 11:27:13 +0300

    stuff

--- routes/event.js
+++ routes/event.js
@@ -1,23 +1,21 @@
 
 var express = require('express')
 var router = express.Router()
-
-var request     = require('request')
+var request = require('request')
 
 router.get('/', function(req, res, next) {
-    //console.log(req.query.id)
     var url = 'https://saal.entu.ee/api2/entity-' + req.query.id
+
     request.get({
         strictSSL: true,
         url: url
         },
         function (err, response, body) {
         	data = JSON.parse(body)
-        	//console.log(data.result)
-            console.log(data.result.displaypicture)
+
             res.render("event", {
                 title: data.result.displayname,
-                image: data.result.properties.photo.values[0].db_value,
+                imageId: data.result.properties.photo.values[0].db_value,
                 content: data.result.properties.description.values[0].value,
                 schedule: data.result.properties.time.values,
                 price: data.result.properties.price.values[0].value,
