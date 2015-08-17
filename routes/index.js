5ce9431de3d31de4e06544fed2b3574d8ca0cf66
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-08-18 01:53:24 +0300

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -8,39 +8,17 @@ var op      = require('object-path')
 
 
 router.get('/', function(req, res, next) {
-    debug('Loading "' + req.url + '"', req.params.lang)
 
     var program = []
-    var upcoming_events = SDC.get('program_upcoming')
-    if (!upcoming_events) {next(new Error('"upcoming_events" not cached...'))}
+    var program = SDC.get('program_upcoming')
+    if (!program) {
+        next(new Error('"program_upcoming" not cached...'))
+    }
 
-            res.render('index', {
-                "program": upcoming_events
-            })
-            res.end()
-            return
-
-    async.each(
-        Object.keys(upcoming_events),
-        function(key, callback) {
-            var event_date = {date:key, events:[]}
-            upcoming_events[key].forEach(function(single_event) {
-                event_date.events.push(single_event)
-            })
-            program.push(event_date)
-            callback()
-        },
-        function(error) {
-            if (error) {
-                debug('2', error)
-            }
-            debug('c',JSON.stringify(program, null, '  '))
-            res.render('index', {
-                "program": program
-            })
-            res.end()
-        }
-    )
+    res.render('index', {
+        "program": program
+    })
+    res.end()
 })
 
 
