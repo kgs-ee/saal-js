817172fa5d7d77d0d3a9a62e4182346d6da9d6aa
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-08-30 14:26:59 +0300

    fix indentation

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,106 +2,104 @@
 **  Events to datepicker
 */
 function datePickerInit() {
-  var baseHref = document.getElementsByTagName('base')[0].getAttribute("href");
-  locale = ((baseHref == '/et/') ? "et" : "en");
+    var baseHref = document.getElementsByTagName('base')[0].getAttribute("href")
+    locale = baseHref.split('/')[1]
+    // locale = ((baseHref == '/et/') ? "et" : "en")
 
-  var options = $.extend(
-      {},
-      $.datepicker.regional[locale],
-      { dateFormat: "d MM, y" }
-  );
-  $.datepicker.setDefaults(options);
+    var options = $.extend(
+        {},
+        $.datepicker.regional[locale],
+        { dateFormat: "d MM, y" }
+    )
+    $.datepicker.setDefaults(options)
 
-  // $( "#datepicker").datepicker({
-  //   beforeShowDay: available,
-  //   prevText: '<',
-  //   nextText: '>',
-  //   weekStart: 1
-  // });
+    // $( "#datepicker").datepicker({
+    //   beforeShowDay: available,
+    //   prevText: '<',
+    //   nextText: '>',
+    //   weekStart: 1
+    // })
 
-var arrEvents = {};
-var currentTitle = "";
+    var arrEvents = {}
+    var currentTitle = ""
+    $.getJSON('http://kgs-ee.github.io/saal-js/javascripts/campo-json.json', function(response) {
+    // $.getJSON(window.location.protocol + '//' + window.location.host + '/public/campo_json.json', function(response){
 
-  
-  $.getJSON('http://kgs-ee.github.io/saal-js/javascripts/campo-json.json', function(response){
-
-    for (var key in response) {
-      //console.log(key);
-      var keys = key.split("-");
-      var d = new Date(keys[0], keys[1] - 1, keys[2]);
-      arrEvents[d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()] = response[key];
-      //console.log(d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate());
-    }
-    
-    //$("#datepicker").html("");
-    $("#datepicker").datepicker({
-      dateFormat : "yy-mm-dd",
-      prevText : '<',
-      nextText : '>',
-      onChangeMonthYear: function(year, month, inst) {
-        // hack default behaviour.
-        setTimeout(function(){
-          toggleTooltip({show:false});
-        }, 0);
-      },
-      onSelect: function(dateText, inst) {
-
-
-        //console.log(dateText);
-        var parts = dateText.split("-");
-        //console.log(parts);
-        var clickedDate = new Date(parts[0], parts[1] - 1, parts[2]);
-        //var clickedDate = new Date(dateText);
-        var dateString = clickedDate.getFullYear() + "-" + (clickedDate.getMonth() + 1) + "-" + clickedDate.getDate();
-        var eventsByDate = arrEvents[dateString];
-        var urlTo = "";
-        
-        if (eventsByDate.length > 1) {
-          urlTo = eventsByDate[0].multiUrl;
-        } else {
-          urlTo = eventsByDate[0].url;
+        for (var key in response) {
+            //console.log(key)
+            var keys = key.split("-")
+            var d = new Date(keys[0], keys[1] - 1, keys[2])
+            arrEvents[d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate()] = response[key]
+            //console.log(d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate())
         }
-        window.location = urlTo;
-      },
-      beforeShowDay: function(date) {
-        
-        var dateString = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-        
-        //console.log(dateString);
-        //console.log(arrEvents);
-        //console.log(arrEvents["2012-9-29"]);
-        var eventsByDate = arrEvents[dateString];
-        
-        var isClickable = false;
-        var extraClass = "";
-        var tooltip = null;
-        
-        if (eventsByDate) {
-          isClickable = true;
-          extraClass = "highlight";
-          if (eventsByDate.length > 1) {
-            extraClass += " multiple"
-          } 
-          tooltip = "<ul class='list-unstyled'>";
-          for (var e in eventsByDate) {
-            var event = eventsByDate[e];
-            /*console.log(event);*/
-            extraClass += " " + event.extraClasses;
-            var time = (event.time != "00:00") ? (event.time + " uur ") : "";
-            tooltip += "<li class='" + event.extraClasses + "'>" + time + ": "  + event.name + ((event.short) ? (" - " + event.short) : "") + "</li>";
-            tooltip += "<li class='" + event.extraClasses + "'>";
-              tooltip += event.name + ((event.short) ? (" - " + event.short) : "") + "<br />";
-              tooltip += event.client + ((event.city) ? (" - " + event.city) : "");
-              tooltip += event.name + ((event.short) ? (" - " + event.short) : "") + "<br />";
-            
-            tooltip += "</li>";
-          }
-          tooltip += "</ul>";
-        }
-        return [isClickable, extraClass, tooltip]
-      }
-    });
-  });
+
+        //$("#datepicker").html("")
+        $("#datepicker").datepicker({
+            dateFormat : "yy-mm-dd",
+            prevText : '<',
+            nextText : '>',
+            onChangeMonthYear: function(year, month, inst) {
+                // hack default behaviour.
+                setTimeout(function(){
+                    toggleTooltip({show:false})
+                }, 0)
+            },
+            onSelect: function(dateText, inst) {
+                //console.log(dateText)
+                var parts = dateText.split("-")
+                //console.log(parts)
+                var clickedDate = new Date(parts[0], parts[1] - 1, parts[2])
+                //var clickedDate = new Date(dateText)
+                var dateString = clickedDate.getFullYear() + "-" + (clickedDate.getMonth() + 1) + "-" + clickedDate.getDate()
+                var eventsByDate = arrEvents[dateString]
+                var urlTo = ""
+
+                if (eventsByDate.length > 1) {
+                    urlTo = eventsByDate[0].multiUrl
+                } else {
+                    urlTo = eventsByDate[0].url
+                }
+                window.location = urlTo
+            },
+            beforeShowDay: function(date) {
+
+                var dateString = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+
+                //console.log(dateString)
+                //console.log(arrEvents)
+                //console.log(arrEvents["2012-9-29"])
+                var eventsByDate = arrEvents[dateString]
+
+                var isClickable = false
+                var extraClass = ""
+                var tooltip = null
+
+                if (eventsByDate) {
+                    isClickable = true
+                    extraClass = "highlight"
+                    if (eventsByDate.length > 1) {
+                        extraClass += " multiple"
+                    }
+                    tooltip = "<ul class='list-unstyled'>"
+                    for (var e in eventsByDate) {
+                        var event = eventsByDate[e]
+                        /*console.log(event);*/
+                        extraClass += " " + event.extraClasses
+                        var time = (event.time != "00:00") ? (event.time + " uur ") : ""
+                        tooltip += "<li class='" + event.extraClasses + "'>" + time + ": "  + event.name + ((event.short) ? (" - " + event.short) : "") + "</li>"
+                        tooltip += "<li class='" + event.extraClasses + "'>"
+                            tooltip += event.name + ((event.short) ? (" - " + event.short) : "") + "<br />"
+                            tooltip += event.client + ((event.city) ? (" - " + event.city) : "")
+                            tooltip += event.name + ((event.short) ? (" - " + event.short) : "") + "<br />"
+
+                        tooltip += "</li>"
+                    }
+                    tooltip += "</ul>"
+                }
+                return [isClickable, extraClass, tooltip]
+            }
+        })
+    })
 }
 
 
@@ -109,12 +107,12 @@ var currentTitle = "";
 **  Calendar tooltip
 */
 function calToolTip() {
-  $('body').tooltip({
-    selector: '[data-handler="selectDay"]',
-    html: true,
-    container: "body",
-    placement: "bottom"
-  });
+    $('body').tooltip({
+        selector: '[data-handler="selectDay"]',
+        html: true,
+        container: "body",
+        placement: "bottom"
+    })
 }
 
 
@@ -122,10 +120,10 @@ function calToolTip() {
 **  Frontpage slider height
 */
 function eventBannerHeight() {
-  windowHeight = $(window).height();
-  navHeight = $('.navbar-default').outerHeight(true);
+    windowHeight = $(window).height()
+    navHeight = $('.navbar-default').outerHeight(true)
 
-  $(".event-banner figure").height(windowHeight - navHeight);
+    $(".event-banner figure").height(windowHeight - navHeight)
 }
 
 
@@ -133,28 +131,28 @@ function eventBannerHeight() {
 **  Programme category dropdown
 */
 function categoryDropDown() {
-  var options = [];
+    var options = []
 
-  $( '.dropdown-menu a' ).on( 'click', function( event ) {
+    $( '.dropdown-menu a' ).on( 'click', function( event ) {
 
-     var $target = $( event.currentTarget ),
-         val = $target.attr( 'data-value' ),
-         $inp = $target.find( 'input' ),
-         idx;
+        var $target = $( event.currentTarget ),
+             val = $target.attr( 'data-value' ),
+             $inp = $target.find( 'input' ),
+             idx
 
-     if ( ( idx = options.indexOf( val ) ) > -1 ) {
-        options.splice( idx, 1 );
-        setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
-     } else {
-        options.push( val );
-        setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
-     }
+        if ( ( idx = options.indexOf( val ) ) > -1 ) {
+            options.splice( idx, 1 )
+            setTimeout( function() { $inp.prop( 'checked', false ) }, 0)
+        } else {
+            options.push( val )
+            setTimeout( function() { $inp.prop( 'checked', true ) }, 0)
+        }
 
-     $( event.target ).blur();
-        
-     console.log( options );
-     return false;
-  });
+        $( event.target ).blur()
+
+        console.log( options )
+        return false
+    })
 }
 
 
@@ -162,22 +160,21 @@ function categoryDropDown() {
 **  Google Maps
 */
 function initMap() {
-  var myLatlng = new google.maps.LatLng(59.438552,24.745975);
-  
-  var mapOptions = {
-    center: myLatlng,
-    zoom: 18
-  };
+    var myLatlng = new google.maps.LatLng(59.438552,24.745975)
 
-  var map = new google.maps.Map(document.getElementById('map'),
-  mapOptions);
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 18
+    }
 
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Kanuti Gildi Saal'
-  });
+    var map = new google.maps.Map(document.getElementById('map'),
+    mapOptions)
 
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Kanuti Gildi Saal'
+    })
 }
 
 
@@ -185,14 +182,14 @@ function initMap() {
 **  Fancybox Gallery
 */
 function fancyBoxGallery() {
-  $(".fancybox").fancybox({
-    padding: 0,
-    helpers: {
-        overlay: {
-          locked: false
+    $(".fancybox").fancybox({
+        padding: 0,
+        helpers: {
+            overlay: {
+                locked: false
+            }
         }
-      }
-  }); 
+    })
 }
 
 
@@ -200,30 +197,30 @@ function fancyBoxGallery() {
 **  Read More generator
 */
 function shortenTexts() {
- $(".main-text").shorten({
-   moreText: 'Loe edasi',
-   lessText: 'Loe vähem',
-   showChars: '1000'
- }); 
+    $(".main-text").shorten({
+        moreText: 'Loe edasi',
+        lessText: 'Loe vähem',
+        showChars: '1000'
+    })
 }
 
 
 
 $(document).ready(function() {
-  datePickerInit();
-  calToolTip();
-  fancyBoxGallery();
-  shortenTexts();
-  // eventBannerHeight();
-  //initMap();
-  categoryDropDown();
-});
+    datePickerInit()
+    calToolTip()
+    fancyBoxGallery()
+    shortenTexts()
+    // eventBannerHeight()
+    //initMap()
+    categoryDropDown()
+})
 
 $(window).load(function() {
-    eventBannerHeight();
-    calToolTip();
-});
+    eventBannerHeight()
+    calToolTip()
+})
 
 $(window).resize(function() {
-    eventBannerHeight();
-});
+    eventBannerHeight()
+})
