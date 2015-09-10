86ad479433a431bc9c139c3fa3df6210775fb19f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-09-10 17:38:49 +0300

    cleanup

--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,7 @@
-var fs      = require('fs')
 var express = require('express')
 var router  = express.Router()
 var path    = require('path')
 var debug   = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
-var async   = require('async')
 var op      = require('object-path')
 
 
