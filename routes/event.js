9d256a8187ab5ed44bb83733ed419b12854a8945
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-09-28 17:57:42 +0300

    event

--- routes/event.js
+++ routes/event.js
@@ -4,13 +4,13 @@ var path    = require('path')
 var debug   = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
 var op      = require('object-path')
 
+var mapper  = require('../helpers/mapper')
 
 router.get('/:id', function(req, res, next) {
     // debug('Looking for', req.query)
     // debug('Looking for', req.path)
 
-    // debug(EVENT_LOOKUP)
-    var event = ALL_EVENTS[EVENT_LOOKUP[req.path.split('/')[1]]]
+    var event = mapper.event(req.path.split('/')[1])
     var coverage = op.get(event, 'coverage', []).concat(op.get(event, 'performance.coverage', []))
     // debug('coverage:', op.get(event, 'coverage', 'N/A'))
     // debug('performance.coverage:', op.get(event, 'performance.coverage', 'N/A'))
