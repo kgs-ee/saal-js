7b970b28c5d0f3278ec6273a54d95a28e0d5c4f9
Ranno Ait <rannoait@gmail.com>
2015-09-30 11:41:27 +0300

    * Program view + route * Sold out button + translation

--- routes/program.js
+++ routes/program.js
@@ -5,6 +5,7 @@ var path    = require('path')
 var debug   = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
 var async   = require('async')
 var op      = require('object-path')
+var mapper  = require('../helpers/mapper')
 
 
 router.get('/', function(req, res, next) {
@@ -12,10 +13,44 @@ router.get('/', function(req, res, next) {
 
     // res.locals.lang = req.params.lang
     res.render('program', {
-        "program": SDC.get('program_upcoming')
+        "program": program_upcoming
     })
     res.end()
 })
 
+router.prepare = function prepare(callback) {
+    debug('Preparing ' + path.basename(__filename).replace('.js', ''))
+    var parallelf = []
+    parallelf.push(prepareUpcomingEvents)
+    async.parallel(parallelf, function(err) {
+        debug('Prepared ' + path.basename(__filename).replace('.js', ''))
+        callback()
+    })
+}
+
+// Upcoming events
+var prepareUpcomingEvents = function prepareUpcomingEvents(callback) {
+    program_upcoming = {}
+    async.each(SDC.get(['local_entities', 'by_class', 'program']), function(entity, callback) {
+        var event = mapper.event(entity.id)
+        // debug(JSON.stringify(event, null, 2))
+        event['start-time'].forEach(function(sttime) {
+            var event_date = (sttime).slice(0,10)
+            var event_time = (sttime).slice(11,16)
+            op.set(event, 'event-date', event_date)
+            op.set(event, 'event-time', event_time)
+            op.push(program_upcoming, [event_date, event_time], event)
+        })
+        callback()
+    }, function(err) {
+        if (err) {
+            debug('Failed to prepare upcoming events.', err)
+            callback(err)
+            return
+        }
+        debug('Upcoming events prepared.')
+        callback()
+    })
+}
 
 module.exports = router
