be54bec35276ee79e3da0cea862991c5f03afb9c
Ranno Ait <rannoait@gmail.com>
2015-10-01 09:56:20 +0300

    tours route

--- routes/tours.js
+++ routes/tours.js
@@ -5,21 +5,52 @@ var path    = require('path')
 var debug   = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
 var async   = require('async')
 var op      = require('object-path')
+var mapper  = require('../helpers/mapper')
 
 
 router.get('/', function(req, res, next) {
+    debug('Loading "' + req.url + '"')
 
-    var tours = []
-    var tours = SDC.get('tours_upcoming')
-    if (!tours) {
-        next(new Error('cache_missing_upcoming_tours'))
-    }
-
+    // res.locals.lang = req.params.lang
     res.render('tours', {
-        "tours": tours
+        "tours": tours_upcoming
     })
     res.end()
 })
 
+router.prepare = function prepare(callback) {
+    debug('Preparing ' + path.basename(__filename).replace('.js', ''))
+    var parallelf = []
+    parallelf.push(prepareUpcomingTours)
+    async.parallel(parallelf, function(err) {
+        debug('Prepared ' + path.basename(__filename).replace('.js', ''))
+        callback()
+    })
+}
+
+// Upcoming tours
+var prepareUpcomingTours = function prepareUpcomingTours(callback) {
+    tours_upcoming = {}
+    async.each(SDC.get(['local_entities', 'by_class', 'tour']), function(entity, callback) {
+        var event = mapper.event(entity.id)
+        // debug(JSON.stringify(event, null, 2))
+        event['start-time'].forEach(function(sttime) {
+            var event_date = (sttime).slice(0,10)
+            var event_time = (sttime).slice(11,16)
+            op.set(event, 'event-date', event_date)
+            op.set(event, 'event-time', event_time)
+            op.push(tours_upcoming, [event_date, event_time], event)
+        })
+        callback()
+    }, function(err) {
+        if (err) {
+            debug('Failed to prepare upcoming tours.', err)
+            callback(err)
+            return
+        }
+        debug('Upcoming tours prepared.')
+        callback()
+    })
+}
 
 module.exports = router
