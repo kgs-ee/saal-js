69e60b5be23831920713a5a9524e97d547d69a03
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-11-16 06:41:22 +0200

    year/month navigation for tours

--- routes/tours.js
+++ routes/tours.js
@@ -6,32 +6,62 @@ var debug   = require('debug')('app:' + path.basename(__filename).replace('.js',
 var async   = require('async')
 var op      = require('object-path')
 var mapper  = require('../helpers/mapper')
+var helper  = require('../helpers/helper')
 
 
-router.get('/', function(req, res, next) {
-    debug('Loading "' + req.url + '"')
-
-    // res.locals.lang = req.params.lang
-    res.render('tours', {
-        "tours": tours_upcoming
+router
+    .get('/', function(req, res, next) {
+        var year = new Date().getUTCFullYear()
+        var month = new Date().getUTCMonth() + 1
+        debug('Assuming ' + year + '/' + month )
+        renderTours(res, year, month, undefined)
+        res.end()
     })
-    res.end()
-})
+    .get('/:year/:month/:categories*?', function(req, res, next) {
+        // debug('Requested "' + req.url + '"' + JSON.stringify(req.params, null, 2))
+        renderTours(res, req.params.year, req.params.month, req.params.categories)
+    })
 
-router.prepare = function prepare(callback) {
-    // debug('Preparing ' + path.basename(__filename).replace('.js', ''))
-    var parallelf = []
-    parallelf.push(prepareUpcomingTours)
-    async.parallel(parallelf, function(err) {
-        // debug('Prepared ' + path.basename(__filename).replace('.js', ''))
+var renderTours = function renderTours(res, year, month, categories) {
+    // console.log('Loading "' + path.basename(__filename).replace('.js', '') + '"')
+
+    tours_a = {}
+    async.each(SDC.get(['local_entities', 'by_class', 'tour']), function(entity, callback) {
+        var event = mapper.event(entity.id)
+        // console.log(JSON.stringify(event, null, 2))
+        if (event['start-time']) {
+
+            if (month - 1 !== new Date(event['start-time']).getUTCMonth()) {
+                // console.log(month - 1, '!==', new Date(event['start-time']).getUTCMonth())
+                return callback()
+            }
+            if (parseInt(year) != new Date(event['start-time']).getUTCFullYear()) {
+                // console.log(year, '!==', new Date(event['start-time']).getUTCFullYear())
+                return callback()
+            }
+
+            var event_date = (event['start-time']).slice(0,10)
+            var event_time = (event['start-time']).slice(11,16)
+            op.set(event, 'event-date', event_date)
+            op.set(event, 'event-time', event_time)
+            op.push(tours_a, [event_date, event_time], event)
+        }
         callback()
+    }, function(err) {
+        if (err) {
+            console.log('Failed to render tours.', err)
+            callback(err)
+            return
+        }
+        res.render('tours', {
+            "monthNav": helper.monthNav(year, month),
+            "tours": tours_a,
+        })
+        res.end()
     })
 }
 
-var tours_upcoming = {}
-
-// Upcoming tours
-var prepareUpcomingTours = function prepareUpcomingTours(callback) {
+router.prepare = function prepare(callback) {
     tours_upcoming = {}
     async.each(SDC.get(['local_entities', 'by_class', 'tour']), function(entity, callback) {
         var event = mapper.event(entity.id)
@@ -50,7 +80,6 @@ var prepareUpcomingTours = function prepareUpcomingTours(callback) {
             callback(err)
             return
         }
-        // debug('Upcoming tours prepared.')
         callback()
     })
 }
