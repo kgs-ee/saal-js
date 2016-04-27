64d2d1ebfaeaea1495705cdd65f0580b98a9a430
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-04-27 17:49:53 +0300

    decluster

--- master.js
+++ master.js
@@ -1,7 +1,13 @@
-var cluster  = require('cluster')
-var cpuCount = require('os').cpus().length
+if(process.env.NEW_RELIC_LICENSE_KEY) { require('newrelic') }
+
+var async    = require('async')
+var bparser  = require('body-parser')
+var cookie   = require('cookie-parser')
+var express  = require('express')
 var fs       = require('fs')
-// var op       = require('object-path')
+var i18n     = require('./helpers/i18n')
+var moment   = require('moment')
+var op       = require('object-path')
 var path     = require('path')
 var debug    = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
 var random   = require('randomstring')
@@ -9,9 +15,11 @@ var raven    = require('raven')
 
 
 APP_ROOT_REFRESH_MS = 30 * 60 * 1000
-APP_COOKIE_SECRET   = process.env.COOKIE_SECRET || random.generate(16)
+APP_COOKIE_SECRET   = random.generate(16)
 APP_DEPLOYMENT      = process.env.DEPLOYMENT
-APP_VERSION         = process.env.VERSION || require('./package').version
+APP_VERSION         = require('./package').version
+APP_DEBUG           = process.env.DEBUG
+APP_PORT            = process.env.PORT || 3000
 
 
 if (!process.env.ENTU_USER) {
@@ -37,25 +45,250 @@ if (!fs.existsSync(APP_CACHE_DIR)) { fs.mkdirSync(APP_CACHE_DIR) }
 var ravenClient = new raven.Client({ release: APP_VERSION })
 
 
-var workers = []
-
 // debug(APP_ENTU_OPTIONS)
+
+
+
+
+
+
+
+
+debug('Loading Entu web ...')
+
+SDC = op({})
+
+var prepareControllersFa = []
+
+prepareControllersFa.push(function loadCache(callback) {
+    var filenames = fs.readdirSync(APP_CACHE_DIR).map(function(filename) {
+        return filename.split('.json')[0]
+    })
+
+    async.each(filenames, function(filename, callback) {
+        // debug({ cmd: 'log', log: 'Loading cache from ' + filename })
+        // debug(filename)
+        // debug('Loading: ', path.join(APP_CACHE_DIR, filename))
+        fs.readFile(path.join(APP_CACHE_DIR, filename + '.json'), function(err, data) {
+            if (err) {
+                debug('Not loaded: ', path.join(APP_CACHE_DIR, filename), err)
+                return callback(err)
+            }
+            debug('filename', filename)
+            try {
+                SDC.set(filename, JSON.parse(data))
+            } catch (e) {
+                console.log('e', e)
+                SDC.set(filename, data)
+            }
+            // debug('Loaded: ', path.join(APP_CACHE_DIR, filename))
+            callback()
+        })
+    }, function(err) {
+        if (err) {
+            debug('Failed to load local cache.', err)
+            debug({ cmd: 'log', log: 'Failed to load local cache.', err: err })
+            callback(err)
+            return
+        }
+        // debug({ cmd: 'log', log: 'Cache loaded.' })
+        // debug('Cache loaded.')
+        callback()
+    })
+})
+
+
+prepareControllersFa.push(function prepareControllers(prepareControllersCB) {
+    // debug({ cmd: 'log', log: 'Preparing data for controllers.' })
+    // debug('Preparing data for controllers.')
+    var controllers = fs.readdirSync(path.join(__dirname, 'routes')).map(function(filename) {
+        return filename.split('.js')[0]
+    })
+    async.eachSeries(controllers, function(controller, callback) {
+        // debug('controller:', controller)
+        var cntrl = require(path.join(__dirname, 'routes', controller))
+        if (cntrl.prepare !== undefined) {
+            // debug({ cmd: 'log', log: 'Preparing ' + controller })
+            cntrl.prepare(function() {
+                // debug({ cmd: 'log', log: 'Callback from ' + controller })
+                callback()
+            })
+        } else {
+            // debug({ cmd: 'log', log: 'Not preparing ' + controller })
+            callback()
+        }
+    }, function(err) {
+        if (err) {
+            // debug('Failed to prepare controllers.', err)
+            debug({ cmd: 'log', log: 'Failed to prepare controllers.', err: err })
+            prepareControllersCB(err)
+            return
+        }
+        // debug({ cmd: 'log', log: 'Controllers prepared.' })
+        debug('Controllers prepared.')
+        prepareControllersCB()
+    })
+})
+
+
+
+
+// Configure i18n
+i18n.configure({
+    locales: ['en', 'et'],
+    defaultLocale: 'et',
+    redirectWrongLocale: true,
+    file: path.join(__dirname, 'localization', 'locales.yaml'),
+    updateFile: true
+})
+
+
+// initialize getsentry.com client
+var ravenClient = new raven.Client({
+    release: APP_VERSION,
+    dataCallback: function(data) {
+        debug(JSON.stringify({data:data}, null, 4))
+        // delete data.request.env
+        return data
+    }
+})
+
+
+var app     = express()
+debug('Start jade engine')
+app
+    // jade view engine
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'jade')
+
+debug('Start getsentry logs')
+app
+    // logs to getsentry.com - start
+    .use(raven.middleware.express.requestHandler(ravenClient))
+
+debug('Cookies')
+app
+    // cookies
+    .use(cookie(APP_COOKIE_SECRET))
+
+debug('Body parser')
+app
+    // parse POST requests
+    .use(bparser.json())
+    .use(bparser.urlencoded({extended: true}))
+
+debug('Static and favicon')
+app
+    // static files path & favicon
+    .use(express.static(path.join(__dirname, 'public')))
+    // .use(favicon(path.join(__dirname, 'public', 'images', 'kgs-logo.ico')))
+
+    // set defaults for views
+    .use(function(req, res, next) {
+        if(req.path === '/') { return res.redirect('/et/') }
+        // debug(JSON.stringify(req.path, null, '    '))
+        // res.locals.lang = 'et'
+        res.locals.moment = moment
+        res.locals.op = op
+        res.locals.SAAL = SDC.get('root')
+        var p = req.path.split('/')
+        p.shift()
+        p.shift()
+        res.locals.path = p.join('/')
+        res.locals.route = p[0]
+        if(req.signedCookies.auth_id && req.signedCookies.auth_token) {
+            res.locals.user = {
+                id: req.signedCookies.auth_id,
+                token: req.signedCookies.auth_token
+            }
+        }
+        next(null)
+    })
+
+    //Initiate i18n
+    .use(i18n.init)
+
+
+
+app
+    // routes mapping
+    .use('/:lang',               require('./routes/index'))
+    .use('/:lang/about/',        require('./routes/about'))
+    .use('/:lang/performance/',  require('./routes/performance'))
+    .use('/:lang/tours/',        require('./routes/tours'))
+    .use('/:lang/program/',      require('./routes/program'))
+    .use('/:lang/co_productions/',      require('./routes/co_productions'))
+    .use('/:lang/residency/',    require('./routes/residency'))
+    .use('/:lang/resident/',     require('./routes/resident'))
+    .use('/:lang/projects/',     require('./routes/projects'))
+    .use('/:lang/contact/',      require('./routes/contact'))
+    .use('/:lang/magazine/',     require('./routes/magazine'))
+    .use('/:lang/search',        require('./routes/search'))
+    // .use('/:lang/signin',        require('./routes/signin'))
+    .use('/:lang/festival',      require('./routes/festival'))
+    .use('/:lang/calendar_json', require('./routes/calendar_json'))
+
+    // logs to getsentry.com - error
+    .use(raven.middleware.express.errorHandler(ravenClient))
+
+    // 404
+    .use(function(req, res, next) {
+        console.log(new Date(), '404:' + req.path)
+        return res.redirect('/')
+
+        // var err = new Error('Not Found')
+        // err.status = 404
+        // next(err)
+    })
+
+    // error
+    .use(function(err, req, res) {
+        var status = parseInt(err.status, 10) || 500
+
+        res.status(status)
+        res.render('error', {
+            title: status,
+            message: res.locals.t('error.' + err.message),
+            error: APP_DEBUG ? err : {}
+        })
+
+        if(err.status !== 404) { debug(err) }
+    })
+
+    // start server
+    .listen(APP_PORT)
+
+
+
+debug('Started at port %s', APP_PORT)
+
+
+
 var cache = require('./helpers/cache')
 cache.sync(
     function report(message, options) {
         options = options || { level: 'info' }
         ravenClient.captureMessage(message, options)
     },
-    function cacheSyncCB() {
-        for (var i in workers) {
-            if (workers.hasOwnProperty(i)) {
-                var worker = workers[i]
-                if (worker) {
-                    console.log('cacheSyncCB: Reload worker ' + worker.id)
-                    worker.send({ cmd: 'reload', dir: APP_CACHE_DIR })
-                }
-            }
+    function reloadCache() {
+        if (SDC.get(['lastPollTs'])) {
+            debug({ cmd: 'log', log: 'Loading local cache from ' + new Date(SDC.get(['lastPollTs'])*1e3) })
+        } else {
+            debug({ cmd: 'log', log: 'Waiting for cache...' })
         }
+
+        debug(280, prepareControllersFa)
+        async.series(prepareControllersFa, function routineFinally(err) {
+            if (err) {
+                debug({ cmd: 'log', log: 'Reload failed', err: err })
+                return
+            }
+            if (SDC.get(['lastPollTs'])) {
+                debug({ cmd: 'log', log: 'Worker reloaded with cache from ' + new Date(SDC.get(['lastPollTs'])*1e3) })
+            } else {
+                debug({ cmd: 'log', log: 'Worker reloaded with no cache.' })
+            }
+        })
     }
 )
 
@@ -81,74 +314,4 @@ if (APP_DEPLOYMENT === 'live' || APP_DEPLOYMENT === 'dev') {
         }
     }
     startPLSync()
-}
-
-
-// Broadcast a message to all other workers
-function broadcast(data, workerId) {
-    if (!workerId) {
-        workerId = -1
-    }
-    console.log('Broadcast from ' + workerId + '.')
-    for (var i in workers) {
-        if (workers.hasOwnProperty(i)) {
-            var worker = workers[i]
-            if (worker && worker.id !== workerId) {
-                worker.send({ cmd: 'data', data: data, from: workerId, chat: 'broadcast to ' + worker.id + ' initiated by ' + workerId })
-            }
-        }
-    }
-}
-
-
-function createWorker() {
-    var worker = cluster.fork()
-    worker.on('message', function(msg) {
-        if (msg.cmd) {
-            switch (msg.cmd) {
-                case 'log':
-                    console.log(new Date().toString() + ' W-' + this.id + ': ' + msg.log)
-                break
-                case 'broadcast':
-                    console.log('W-' + this.id + ' broadcasting...')
-                    broadcast(msg.data, this.id)
-                break
-            }
-        }
-        if (msg.err) {
-            console.log(JSON.stringify({worker: this.id, error: err}, null, 2))
-        }
-    })
-    // Add the worker to an array of known workers
-    workers[worker.id] = worker
-}
-
-cluster.setupMaster({ exec: path.join(__dirname, 'worker.js') })
-
-if (cluster.isMaster) {
-    // Create a worker for each CPU
-    cpuCount = 1
-    for (var i = 0; i < cpuCount; i += 1) {
-        createWorker()
-    }
-
-    // Listen for new workers
-    cluster.on('online', function(worker) {
-        console.log(new Date().toString() + ' worker ' + worker.id + ' started')
-        worker.send({ cmd: 'APP_COOKIE_SECRET', APP_COOKIE_SECRET: APP_COOKIE_SECRET })
-        worker.send({ cmd: 'reload', dir: APP_CACHE_DIR })
-    })
-
-    // Listen for dying workers nad replace the dead worker, we're not sentimental
-    cluster.on('exit', function(worker, code, signal) {
-        workers[worker.id] = false
-        if(signal) {
-            console.error('Worker #' + worker.id + ' was killed by signal: ' + signal)
-        } else if(code !== 0) {
-            console.error('Worker #' + worker.id + ' exited with error code: ' + code)
-        } else {
-            console.error('Worker #' + worker.id + ' success!')
-        }
-        createWorker()
-    })
 }
