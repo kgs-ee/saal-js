b46906f8ebdcc63e0cdd263da330a1febae1e46d
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-09-05 08:37:09 +0300

    setenv.js

---
+++ helpers/setenv.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+var random = require('randomstring')
+var path = require('path')
+
+APP_ROOT_DIR = path.join(__dirname, '..')
+
+APP_ROOT_REFRESH_MS = 30 * 60 * 1000
+APP_COOKIE_SECRET = random.generate(16)
+APP_DEPLOYMENT = process.env.DEPLOYMENT
+APP_VERSION = require(path.join(APP_ROOT_DIR, 'package.json')).version
+APP_DEBUG = process.env.DEBUG
+APP_PORT = process.env.PORT || 3000
+ENTU_POLL_SEC = process.env.ENTU_POLL_SEC * 1e3 || 10e3
+
+APP_ENTU_OPTIONS = {
+  entuUrl: process.env.ENTU_URL || 'https://saal.entu.ee',
+  user: process.env.ENTU_USER,
+  key: process.env.ENTU_KEY
+}
+
+// Site data cache
+APP_ENTU_ROOT = 1 // institution
+APP_CACHE_DIR = path.join(APP_ROOT_DIR, 'pagecache')
+if (!fs.existsSync(APP_CACHE_DIR)) { fs.mkdirSync(APP_CACHE_DIR) }
+
+if (!process.env.ENTU_USER) {
+  throw '"ENTU_USER" missing in environment'
+}
+if (!process.env.ENTU_KEY) {
+  throw '"ENTU_KEY" missing in environment'
+}
+
+console.log('\n\nEnv loaded for', APP_ROOT_DIR, APP_VERSION)
